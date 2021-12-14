47576a60b694ae57456bba97e9de5a1e77554ca7
JeHwanYoo <yoojehwan1875@gmail.com>
2021-12-14 12:03:03 +0900

    db 얹히기 성공

--- api/accounts.js
+++ api/accounts.js
@@ -1,21 +1,25 @@
 import { Router } from "express";
 import { Accounts } from "../models";
+import bcrypt from "bcrypt";
+import randomstring from "randomstring";
 
 const router = Router();
-
-/*
-가입 요청	/account
-userid 중복 확인	/users/:userid
-이메일 중복 확인	/emails/:email
-*/
 
 /** userid 중복확인 */
 router.get("/users/:userid", async (req, res) => {
   const userid = req.params["userid"];
 
   try {
-    const accounts = await Accounts.findAll();
-    res.send("doo");
+    const accounts = await Accounts.findOne({
+      where: {
+        userid,
+      },
+    });
+    if (accounts.length > 0) {
+      res.send({ result: true });
+    } else {
+      res.send({ result: false });
+    }
   } catch (e) {
     res.status(400);
     res.send(e);
@@ -23,6 +27,54 @@ router.get("/users/:userid", async (req, res) => {
 });
 
 /** 이메일 중복확인 */
+router.get("/emails/:email", async (req, res) => {
+  const email = req.params["email"];
+
+  try {
+    const accounts = await Accounts.findOne({
+      where: {
+        email,
+      },
+    });
+    if (accounts.length > 0) {
+      res.send({ result: true });
+    } else {
+      res.send({ result: false });
+    }
+  } catch (e) {
+    res.status(400);
+    res.send(e);
+  }
+});
+
 /** 가입 요청 */
+router.post("/", (req, res) => {
+  const { userid, email, name, password } = req.body;
+
+  bcrypt.hash(password, 10, async (err, salt) => {
+    if (err) {
+      res.status(400);
+      res.send(err);
+      return;
+    }
+    try {
+      const account = await Accounts.create({
+        userid,
+        email,
+        name,
+        password,
+      });
+      if (account) {
+        res.sendStatus(200);
+      } else {
+        res.status(400);
+        res.send(e);
+      }
+    } catch (e) {
+      res.status(400);
+      res.send(e);
+    }
+  });
+});
 
 export default router;
--- models/accounts.js
+++ models/accounts.js
@@ -38,6 +38,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: "Accounts",
+      underscored: true,
     }
   );
   return Accounts;
